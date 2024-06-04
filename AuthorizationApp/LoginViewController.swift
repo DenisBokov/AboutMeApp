d74652e9dc377dba89c898e20ed15e4c2e593b5e
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-06-04 21:12:19 +0300

    Create project and LoginViewController

--- AuthorizationApp/LoginViewController.swift
+++ AuthorizationApp/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  AuthorizationApp
 //
 //  Created by Denis Bokov on 04.06.2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
