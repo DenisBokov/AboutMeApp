be33c945d496e38eda9db7e7325036127cc755cc
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-06-04 21:18:20 +0300

    Change text field

--- AuthorizationApp/LoginViewController.swift
+++ AuthorizationApp/LoginViewController.swift
@@ -16,6 +16,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         
         userNameTextField.autocorrectionType = .no
+        userNameTextField.keyboardType = .numberPad
     }
 
 
