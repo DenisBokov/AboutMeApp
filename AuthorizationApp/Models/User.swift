62aaeb7b67ccc9a1547c40666b371057d4ec426d
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-06-06 23:20:33 +0300

    Create user model

--- AuthorizationApp/Models/User.swift
+++ AuthorizationApp/Models/User.swift
@@ -5,4 +5,13 @@
 //  Created by Denis Bokov on 06.06.2024.
 //
 
-import Foundation
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+}
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
