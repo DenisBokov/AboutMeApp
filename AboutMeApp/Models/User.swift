98e069f6b9b24b44d22ff9f7955affcba3ec55df
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2024-06-09 22:42:51 +0300

    Description about me

--- AboutMeApp/Models/User.swift
+++ AboutMeApp/Models/User.swift
@@ -9,9 +9,20 @@ struct User {
     let login: String
     let password: String
     let person: Person
+    
+    static func getPerson() -> Person {
+        Person(
+            firstName: "Денис",
+            lastName: "Боков",
+            personImage: "",
+            description: "Родился 27 июля 87 года в г. Горьком"
+        )
+    }
 }
 
 struct Person {
     let firstName: String
     let lastName: String
+    let personImage: String
+    let description: String
 }
